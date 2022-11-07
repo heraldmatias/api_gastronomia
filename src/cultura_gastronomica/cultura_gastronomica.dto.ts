8696c2f8bfc8bc4b76b227302f5e4a70ce33f962
Herald Olivares <heraldmatias.oz@gmail.com>
2022-11-06 19:05:27 -0500

    Solvin one code smell to se quality gate update

--- src/cultura_gastronomica/cultura_gastronomica.dto.ts
+++ src/cultura_gastronomica/cultura_gastronomica.dto.ts
@@ -1,11 +1,10 @@
-import {IsNotEmpty, IsString, IsUrl} from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CulturaGastronomicaDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly nombre: string;
 
-    @IsString()
-    @IsNotEmpty()
-    readonly nombre: string;
-
-    @IsString()
-    @IsNotEmpty()
-    readonly descripcion: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly descripcion: string;
 }
